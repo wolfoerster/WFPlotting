1b02cc96861505ad5f5ff489407372150e32e171
Wolfgang Förster <wolfoerster@gmx.de>
2026-03-03 22:51:03 +0100

    Second commit

---
+++ WFPlotting/Aaron.cs
@@ -0,0 +1,20 @@
+﻿//******************************************************************************************
+// Copyright © 2026 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the WFPlotting project which can be found on github.com.
+//
+// WFPlotting is free software: you can redistribute it and/or modify it under the
+// terms of the MIT License as published by the Massachusetts Institute of Technology
+// (<https://www.mit.edu/~amini/LICENSE.md>).
+//******************************************************************************************
+using System;
+
+namespace WFPlotting;
+
+public static class Aaron
+{
+    public static void SayHello()
+    {
+        Console.WriteLine("Hello, my name is Aaron!");
+    }
+}
